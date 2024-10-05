81e07c9f2d8dcea1b0707c416f680cfa9d3c168b
Robin Lee Powell <robinleepowell@gmail.com>
2024-10-05 00:07:04 -0700

    Script cleanup
    - error handling blurb
    - echo statement to show completion
    - shellcheck
    - don't work on directories with no new stuff

--- misc/mhonarc_update.sh
+++ misc/mhonarc_update.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+# Error trapping from https://gist.github.com/oldratlee/902ad9a398affca37bfcfab64612e7d1
+__error_trapper() {
+  local parent_lineno="$1"
+  local code="$2"
+  local commands="$3"
+  echo "error exit status $code, at file $0 on or near line $parent_lineno: $commands"
+}
+trap '__error_trapper "${LINENO}/${BASH_LINENO}" "$?" "$BASH_COMMAND"' ERR
+
+set -euE -o pipefail
+shopt -s failglob
 
 days="$1"
 shift
 
-while [ "$1" ]
+while [ "${1:-}" ]
 do
   # Takes the name of a list and pulls posts from the plain text archives into mhonarc.
 
@@ -12,18 +24,19 @@ do
 
   export PATH=/bin:/usr/bin
 
-  posts=$(find /srv/lojban/plain/$list -type f -mtime -$days | grep -v procmail.log)
-  count=$(echo $posts | wc -w)
-  postdirs=$(find /srv/lojban/plain/$list -type f -mtime -$days -printf "%h\n" | grep -v '/rejected' | sort | uniq)
-  pdcount=$(echo $postdirs | wc -w)
+  set +o pipefail
+  count="$(find "/srv/lojban/plain/$list" -type f -mtime "-$days" | grep -v procmail.log | grep -v '\.zip$' | wc -l)"
+  postdirs=$(find "/srv/lojban/plain/$list" -type f -mtime "-$days" -printf "%h\n" | grep -v '/rejected' | sort | uniq)
+  set -o pipefail
+  pdcount=$(echo "$postdirs" | wc -w)
 
-  if [ $count -gt 0 ]
+  if [[ $count -gt 0 ]]
   then
-    echo "Post count for $list is $count in $pdcount directories"
+    echo "New post count for $list is $count in $pdcount directories"
     for dir in $postdirs
     do
       # -mhpattern is from http://www.red-bean.com/doc/mhonarc/faq/usage.html#sepfiles
-      /usr/bin/mhonarc -main -add -idxfname index.html -idxsize 100 -multipg -rcfile /usr/local/etc/mhonarc/indexer.mrc -outdir /srv/lojban/web/$list -mhpattern "^[0-9]" $dir 2>&1 | \
+      /usr/bin/mhonarc -main -add -idxfname index.html -idxsize 100 -multipg -rcfile /usr/local/etc/mhonarc/indexer.mrc -outdir "/srv/lojban/web/$list" -mhpattern "^[0-9]" "$dir" 2>&1 | \
       sed 's/\.\.\.*/\./g' | \
       grep -v 'defined.%hash. is deprecated at' | \
       grep -v 'Maybe you should just omit the defined' | \
@@ -34,5 +47,9 @@ do
       grep -v 'No new messages' | \
       grep -v '^\s*$'
     done
+  else
+      echo "No new posts found for $list"
   fi
 done
+
+echo "Done with mhonarc_update run"
